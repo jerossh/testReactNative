6cc50103ddf67903a721a2b73b780d60321c022c
jerossh <jerosshen@gmail.com>
2017-03-25 14:05:25 +0800

    布局好像有点入门了

--- app/pages/Discover.js
+++ app/pages/Discover.js
@@ -29,25 +29,25 @@ export default class Discover extends Component {
     _renderHeader() {
       return ( // 按照文档流所以这个药在最下方，才能覆盖最上面的
         <View style={styles.header}>
-           <Text style={[styles.backBtn,{opacity: 0}]}>
-             关闭
-           </Text>
-           <TouchableWithoutFeedback>
-             <View>
-                <Text style={styles.searchTextBtn}>搜索文章或名片信息</Text>
-             </View>
-           </TouchableWithoutFeedback>
-
+           <View style={[styles.backBtn,{flex: 1}]}>
+              <Icon name='ios-add-circle-outline' color='white' size={24} />
+           </View>
+           <View style={[styles.searchTextBtn ,{flex: 5}]}>
+                <Text style={{color: 'white', opacity: 1}}>搜索文章或名片信息</Text>
+           </View>
+           <View style={[styles.backBtn,{flex: 1}]}>
+             <Icon name='ios-alert-outline' color='white' size={24} />
+            </View>  
         </View>
       );
     }
     _qkZone() {
       const w = width/4, h = w*.6 + 20
       const qk4 = [
-        ['扫一扫', 'qrcode'],
-        ['精选文章', 'icon'],
-        ['广告设置', 'icon'],
-        ['我的名片', 'icon']
+        ['扫一扫', 'ios-crop-outline'],
+        ['精选文章', 'ios-book-outline'],
+        ['广告设置', 'ios-briefcase-outline'],
+        ['我的名片', 'ios-card-outline']
       ];
       return (
         <View style={styles.qkZone}>
@@ -56,7 +56,7 @@ export default class Discover extends Component {
                return (
                  <TouchableWithoutFeedback key={index}>
                    <View style={{ alignItems: 'center'} /* 为什么可用 ？ */}>
-                      <Icon name="ios-person" size={30} color="white" />
+                      <Icon name={item[1]} size={30} color="white" />
                       <Text style={{color: 'white', textAlign: 'center', fontSize: px2dp(13), paddingTop: 10}}>{item[0]}</Text>
                    </View>
                  </TouchableWithoutFeedback>
@@ -97,29 +97,32 @@ const styles = StyleSheet.create({
     top: 0,
     // bottom:0, // 这个是全凭渲染
     backgroundColor: '#38f',
-    paddingHorizontal: 40,
+    paddingHorizontal: 10,
     paddingTop: isIOS ? 30 : 10, // 安卓状态栏自动留出，苹果不会
     paddingBottom: 10,
-    // justifyContent: 'center',
-    flexDirection: 'row'
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    alignItems: 'stretch', // 没有 拉伸效果，子项目还是要设置高度
+    // height: px2dp(30) + 30，
   },
-  backBtn: {color: 'white', fontSize: 14,
-    backgroundColor: 'green',
-    textAlign: 'center',
-    marginRight: px2dp(10)
+  backBtn: {
+    height: px2dp(30), // 至少一个高度？
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 5
   }, 
   searchTextBtn: {
     backgroundColor: 'white',
+    opacity: 0.4,
     paddingHorizontal: 10,
-    width: px2dp(280),
-    fontSize: px2dp(14),
-    textAlign: 'center',
-    borderRadius: px2dp(3)
+    borderRadius: 5,
+    marginHorizontal: px2dp(10),
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   qkZone: {
     paddingTop: px2dp(isIOS?120:100),
     paddingBottom: 10,
-    // height: 100,
     flex: 1,
     backgroundColor: "#38f",
     flexDirection: "row",
